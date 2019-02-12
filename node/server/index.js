fd9079aaa93b7d8bcc16e1a14a066f9bf5912696
RobinTHOREL <thorelrobin@yahoo.fr>
2019-02-12 12:15:41 +0100

    second status

--- node/server/index.js
+++ node/server/index.js
@@ -1,18 +1,8 @@
-const Movie = require('./models/movie');
+const express = require('express');
+const movieRouter = require('./routes/movie')
 
-const movie1 = new Movie();
-movie1.title = 'Test Movie';
-movie1.year = 1990;
-movie1.released = Date.now();
+const app = express();
 
-movie1.save(function(error, result) {
-    console.log('save');
-    console.log(error);
-    console.log(result);
-});
+app.use('/movies', movieRouter); //define patern for the router
 
-const fetchMovies = Movie.find({title: "Test Movie"}, function(error, result) {
-    console.log('find');
-    console.log(result);
-    console.log(error);
-})
+app.listen(3000, () => console.log('Listening on port 3000'));
