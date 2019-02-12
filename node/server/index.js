546fc37456355b3df49312a904ff1ea2a7150bd1
RobinTHOREL <thorelrobin@yahoo.fr>
2019-02-12 16:52:32 +0100

    add jwt

--- node/server/index.js
+++ node/server/index.js
@@ -4,12 +4,18 @@ const movieRouter = require('./routes/movie');
 const productRouter = require('./routes/product');
 const userRouter = require('./routes/user');
 const categoryRouter = require('./routes/category');
+const securityRouter = require('./routes/security');
 
 const bodyParser = require('body-parser');
+
+const security = require('./middlewares/security');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(security.verifyToken);
+app.use(securityRouter)
 
 app.use('/movies', movieRouter);
 app.use('/products', productRouter);
