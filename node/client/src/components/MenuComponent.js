5dd6daa67677126932b0a2957133a8ef16c7315c
RobinTHOREL <thorelrobin@yahoo.fr>
2019-02-14 14:35:57 +0100

    solve import fix and work on button redirect

--- node/client/src/components/MenuComponent.js
+++ node/client/src/components/MenuComponent.js
@@ -1,21 +1,15 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom'
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
+import { Drawer, CssBaseline, AppBar, Toolbar, List, Link, Typography, IconButton, ListItem,
+        ListItemText, ListItemIcon, Button, Divider } from '@material-ui/core';
+
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+//import ListItemIcon from '@material-ui/core/ListItemIcon';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 
@@ -93,6 +87,11 @@ class PersistentDrawerLeft extends React.Component {
     this.setState({ open: false });
   };
 
+  handleRedirect = (target) => {
+    //this.props.history.push(target);
+  }
+    
+
   render() {
     const { classes, theme } = this.props;
     const { open } = this.state;
@@ -142,9 +141,9 @@ class PersistentDrawerLeft extends React.Component {
                 <ListItemText primary={text} />
               </ListItem>
             ))}
-            <ListItem button to='/movies'>
+            <ListItem>
                 <ListItemIcon> <InboxIcon /> </ListItemIcon>
-                <ListItemText primary={"Objets en vente"} />
+                <ListItemText onClick={this.handleRedirect('/products')} primary={"Objets en vente"} />
               </ListItem>
           </List>
         </Drawer>
